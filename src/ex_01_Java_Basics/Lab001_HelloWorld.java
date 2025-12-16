233b1b05f8f0c4ea7a4388df65ed08a7e5c79df2
saurabhtekale <saurabh.tekale@gmail.com>
2025-12-16 22:39:05 +0530

    Added new changes having no method

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -1,4 +1,12 @@
 package ex_01_Java_Basics;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println();
+    }
 }
+
+
---
+++ src/ex_02_Java_Basics/Lab003_NoMethod.java
@@ -0,0 +1,7 @@
+package ex_02_Java_Basics;
+
+public class Lab003_NoMethod {
+    public static void main() {
+        System.out.println("This method added by own, not default");
+    }
+}
